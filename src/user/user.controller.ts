b7fc31078d4da8259f5a1c51f6a54228236008b2
Andry Pebrianto <andrypeb227@gmail.com>
2024-08-29 15:21:52 +0700

    HTTP request

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('/api/users')
 export class UserController {
@@ -10,5 +11,20 @@ export class UserController {
   @Get('/sample')
   get(): string {
     return 'GET';
+  }
+
+  @Get('/:id')
+  getById(@Req() req: Request): string {
+    return `GET ${req.params.id}`;
+  }
+
+  @Get('/param/:id')
+  getByParam(@Param('id') id: string): string {
+    return `PARAM: ${id}`;
+  }
+
+  @Get('/query/query')
+  getByQuery(@Query('search') search: string): string {
+    return `QUERY: ${search}`;
   }
 }
